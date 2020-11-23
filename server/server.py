464e2bfdf4ab0bc71ab5539eebed885926450936
gakalaba <akalaba@princeton.edu>
2020-11-23 18:22:31 -0500

    untested server that accepts and shares URLs to clients and a random index.html

--- server/server.py
+++ server/server.py
@@ -1,29 +1,55 @@
-import socket
-import sys
+'''Baseline code taken from https://wiki.python.org/moin/BaseHttpServer'''
 
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from io import BytesIO
 
-# Bind the socket to the address given on the command line
-# E.g. on an Amazon EC2 instance with port 10000 open to anywhere
-# $ python server.py ec2-3-22-221-12.us-east-2.compute.amazonaws.com
-server_name = sys.argv[1]
-server_address = (server_name, 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-sock.bind(server_address)
-sock.listen(1)
+HOST_NAME = ""
+PORT_NUMBER = 8080
 
-while True:
-    print >>sys.stderr, 'waiting for a connection'
-    connection, client_address = sock.accept()
+URL_hash_table = dict()
+
+class HTTPRequestHandler(BaseHTTPRequestHandler):
+        def do_HEAD(self):
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+
+        def do_GET(self):
+            '''Respond to a GET request'''
+            self.send_response(200)
+            # self.send_header("Content-type", "text/html")
+            self.end_headers()
+            '''Send over content'''
+            (client_host, client_port) = self.client_address
+            URL = URL_hash_table.get(client_host, "")
+            self.wfile.write(URL)
+            '''Remove the client_host-URL key-value pair from the hash table'''
+            if (URL != ""): 
+                URL_hash_table.pop(client_host)
+
+        def do_POST(self):
+            '''Respond to a PUT request'''
+            content_length = int(self.headers['Content-Length'])
+            body = self.rfile.read(content_length)
+            self.send_response(200)
+            self.end_headers()
+            response = BytesIO()
+            response.write(b'This is POST request. ')
+            response.write(b'Received: ')
+            response.write(body)
+            self.wfile.write(response.getvalue())
+            '''Add the shared tab in the dictionary'''
+            target_host = body.split(":")[0]
+            URL = body.split(":")[1]
+            URL_hash_table[target_host] = URL
+
+
+if __name__ == '__main__':
+    httpd = HTTPServer((HOST_NAME, PORT_NUMBER), HTTPRequestHandler)
+    print time.asctime(), "Server Startes = %s:%s" % (HOST_NAME, PORT_NUMBER)
     try:
-        print >>sys.stderr, 'client connected:', client_address
-        while True:
-            data = connection.recv(16)
-            print >>sys.stderr, 'received "%s"' % data
-            if data:
-                connection.sendall(data)
-            else:
-                break
-    finally:
-        connection.close()
+        httpd.server_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print time.asctime(), "Server Stops - %s:%s" % (HOST_NAME, PORT_NUMBER)
